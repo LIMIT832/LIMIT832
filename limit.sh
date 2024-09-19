fc10ff348c469354a4bfaefc95e5a6b881d8c035
LIMIT832 <ekeke4007@gmail.com>
2024-09-19 16:10:25 +0700

    Create limit.sh

---
+++ limit.sh
@@ -0,0 +1,123 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='y \033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+figlet WELCOME
+echo
+sleep 1
+toilet -f big -F gay -F border LIMIT
+
+echo $red"Silahkan pilih tools nya :"
+echo
+echo $blue"1:liat ip anda"
+echo
+echo $yellow"2:rubikphish"
+echo
+echo $purple"3:GhostTrack"
+echo
+echo $blue"4:OTP_TeRoRV2 "
+echo
+echo $blue"5:Virus "
+echo
+echo $red"6:Spam SMS"
+echo
+echo $purpel"7:nomor pembuat"
+echo
+echo $blue"8:Auto Token01"
+echo
+echo $red"9:Auto token02"
+echo
+echo $green"0:Keluar"
+echo
+read -p "PilihMana :" bro
+
+if [ $bro = 1 ] || $bro = 1 ]
+then
+clear
+echo "Masukkan IP yang ingin dilacak:"
+clear echo "Tracking IP
+$ip..."
+
+    curl "http://ipinfo.io/$ip"
+echo
+else
+    echo "Pilihan tidak valid!"
+fi
+
+if [ $bro = 2 ] || $bro = 2 ]
+then
+clear                                                                                                                                                                            git clone https://github.com/rubikproxy/rubikphish.git
+ cd rubikphish
+ chmod +x rubikphish.sh
+ bash rubikphish.sh
+fi
+if [ $bro = 3 ] || $bro = 3 ]
+then
+clear
+echo "Meng-clone GhostTrack dan menginstal dependensi..."
+            git clone https://github.com/HunxByts/GhostTrack.git
+            cd GhostTrack
+            pip3 install -r requirements.txt
+            python3 GhostTR.py
+           fi
+if [ $bro = 4 ] || $bro = 4 ]
+then
+clear
+echo "Meng-clone OTP_TeRoRV2 dan menjalankan make..."
+            git clone https://github.com/Bayu12345677/OTP_TeRoRV2.git
+            cd OTP_TeRoRV2
+            make Run
+            cd ..
+           fi
+if [ $bro = 5 ] || $bro = 5 ]
+then
+clear
+git clone https://github.com/Projeckerror/Virus
+cd Virus
+bash Virus.sh
+fi
+
+if [ $bro = 6 ] || $bro = 6 ]
+then
+clear
+git clone https://github.com/DomathID/spam-sms.git
+cd spam-sms
+php gas.php
+fi
+if [ $bro = 7 ] || $bro = 7 ]
+then
+clear
+sleep 1
+echo
+echo $red"+6285767506441"  $blue"Jangan aneh aneh"
+echo
+fi
+
+if [ $bro = 8 ] || $bro = 8 ]
+then
+clear
+sleep 2
+echo
+echo  $yellow"NNSWY5LBOJTWCIDjjddlamdnakdnajdpqdjqqjejjijid973371hajdhadjh"
+echo
+fi
+
+if [ $bro = 9 ] || $bro = 9 ]
+then
+clear
+sleep 2
+echo
+echo  $red"NNSWJSANSLANDALkoewjbkanwjwbhftdkdpoikjhyuujnmbgh"
+echo
+fi
+
+if [ $bro = 0 ] || $bro = 0 ]
+then
+clear
+echo
+clear
+fi
